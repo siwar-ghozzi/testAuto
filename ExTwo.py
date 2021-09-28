335d150b725c7b03019586ec784fdec9f9c7d592
Siwar Ghozzi <ghozzisiwar90@gmail.com>
2021-09-28 14:10:49 +0100

    ajout du premier exercice2 (search and click enter with 2 ways)

---
+++ ExTwo.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(executable_path="D:\webdriver\chromedriver.exe")
+driver.get('https://www.google.tn/')
+driver.find_element_by_name("q").send_keys("selenium", Keys.ENTER)
